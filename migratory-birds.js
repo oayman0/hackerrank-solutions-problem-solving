f9abf6608434669e40fcbe165b5d6a5ffe0003f1
Omar Ayman <omaraymankhalaf@gmail.com>
2023-01-25 16:46:10 +0200

    new solution

--- migratory-birds.js
+++ migratory-birds.js
@@ -1,5 +1,35 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'migratoryBirds' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
 function migratoryBirds(arr) {
-    // Write your code here
 arr=arr.sort((a,b)=>{return a-b});
 var obj={};
 arr.forEach(function(item){
@@ -7,6 +37,28 @@ if (!obj[item]){obj[item]=1}else{
 obj[item]+=1    
 }    
 })
-    
+var x=0;
+var y=0;
+for(var i=0;i<arr.length;i++){
+if (obj[arr[i]]>x){
+    x=obj[arr[i]];
+    y=arr[i];
+}
+}
+    console.log(arr,obj,x,y)
+    return y;
+}
 
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const arrCount = parseInt(readLine().trim(), 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    const result = migratoryBirds(arr);
+
+    ws.write(result + '\n');
+
+    ws.end();
 }
