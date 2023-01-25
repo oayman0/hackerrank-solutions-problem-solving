74ebdc5f4d0aa84bcea678520a732c02c589b355
Omar Ayman <omaraymankhalaf@gmail.com>
2023-01-25 18:00:25 +0200

    new solution

---
+++ sales-by-match.js
@@ -0,0 +1,65 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'sockMerchant' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER_ARRAY ar
+ */
+
+function sockMerchant(n, arr) {
+   // Write your code here
+    
+    arr=arr.sort((a,b)=>{return a-b});
+var obj={};
+arr.forEach(function(item){
+if (!obj[item]){obj[item]=1}else{
+obj[item]+=1    
+}    
+})
+var pairs =0;
+for (const key in obj){
+    pairs+=Math.floor(obj[key]/2);
+}
+
+console.log(arr,obj,pairs)
+return pairs;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const ar = readLine().replace(/\s+$/g, '').split(' ').map(arTemp => parseInt(arTemp, 10));
+
+    const result = sockMerchant(n, ar);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
