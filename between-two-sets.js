c3b7b5c4d28cd36746508ac806ba19c053153b79
Omar Ayman <omaraymankhalaf@gmail.com>
2023-02-07 05:41:49 +0200

    new solutions

---
+++ between-two-sets.js
@@ -0,0 +1,46 @@
+function getTotalX(a, b) {
+var arr=[];
+var final=0;
+for( var lg of b){
+for (sm of a){
+    arr.push(lg/sm);
+    arr.push(sm)
+}
+arr.push(lg)}
+console.log(arr)
+
+var set1={}
+for (var z of arr){
+    if(!set1[z]){
+        set1[z]=true
+    }
+}
+
+console.log(set1)
+
+for( var setElem in set1){
+var countA=0
+var countB=0
+for(var sm of a){
+    if(setElem%sm==0){
+countA++
+    }
+}
+for(var lg of b){
+    if(lg%setElem==0){
+countB++
+    }
+}
+console.log(countA,countB)
+if (countA===a.length && countB===b.length){
+final++
+}
+}
+console.log(final)
+
+return final
+
+}//end of function
+var a= [1]
+var b= [72,48]
+getTotalX(a,b);
---
+++ counting-valleys.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'countingValleys' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER steps
+ *  2. STRING path
+ */
+
+function countingValleys(steps, path) {
+    var count=0;
+    var downs=0;
+    for (var i=0;i<path.length;i++){
+        if (path[i]==='U'){
+            count++
+        }else{
+            count--
+        }
+        console.log(i,count)
+        if ((count===0)&&(path[i]==='U')){
+            downs++
+            }
+    }
+    return downs
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const steps = parseInt(readLine().trim(), 10);
+
+    const path = readLine();
+
+    const result = countingValleys(steps, path);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
