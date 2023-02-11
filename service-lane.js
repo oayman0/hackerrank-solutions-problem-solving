a7e6a7108554d7c81a685ee58598c1b6cccecbd7
Omar Ayman <omaraymankhalaf@gmail.com>
2023-02-11 21:56:13 +0200

    new solution

---
+++ service-lane.js
@@ -0,0 +1,75 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'serviceLane' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. 2D_INTEGER_ARRAY cases
+ */
+
+function serviceLane(n, cases, width) {
+    // Write your code here
+    var arr1=[]
+    var arr2=[]
+        for (var i = 0; i < cases.length; i++) {
+       var enter = cases[i][0]
+       console.log(enter);
+       var exit = cases[i][1]
+       console.log(exit);
+    arr1 = width.slice( enter, exit+1)
+    console.log(arr1);
+    arr1 = arr1.sort( ( a, b)=>{ return a-b})
+    console.log(arr1);
+    arr2.push(arr1[0])
+    console.log(arr2);
+    }
+   return arr2
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+    const n = parseInt(firstMultipleInput[0], 10);
+
+    const t = parseInt(firstMultipleInput[1], 10);
+
+    const width = readLine().replace(/\s+$/g, '').split(' ').map(widthTemp => parseInt(widthTemp, 10));
+
+    let cases = Array(t);
+
+    for (let i = 0; i < t; i++) {
+        cases[i] = readLine().replace(/\s+$/g, '').split(' ').map(casesTemp => parseInt(casesTemp, 10));
+    }
+
+    const result = serviceLane(n, cases, width);
+
+    ws.write(result.join('\n') + '\n');
+
+    ws.end();
+}
